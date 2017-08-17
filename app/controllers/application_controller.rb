dafa896272ec9656980ef7d4d946d3a381f293fc
Alexander Oleynik <x103103@gmail.com>
2017-08-17 15:29:09 +0300

    root action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  def root
+    render text: 'This is the root action'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root to: 'application#root'
   mount_devise_token_auth_for User.name, at: 'api/v1/auth'
 end
